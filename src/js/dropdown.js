aace38f00f64e5e991a38bdb42724ca0de12d701
WhiteMooncy <167587307+WhiteMooncy@users.noreply.github.com>
2025-04-27 22:18:52 -0400

    update

--- src/js/dropdown.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-link').addEventListener('click', function (event) {
-    event.preventDefault();
-    const dropdown = document.getElementById('contact-dropdown');
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-});
